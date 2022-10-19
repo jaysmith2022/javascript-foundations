7f33f014c1fde62c46badcb16b1323dd93e5ee72
jordan smith <jordan.smith6035@gmail.com>
2022-10-19 15:26:34 -0600

    Complete crafting test

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,23 @@
 class Craft {
+    constructor(artType) {
+        this.name = artType.type
+        this.materials = artType.materials
+        this.completed = false
+    }
 
+    completeCraft() {
+      this.completed = true
+      return 'All done! It looks great!'
+    }
+
+    calculateProjectTotal() {
+       var cost = 0
+
+       for (var i = 0; i < this.materials.length;i++) {
+        cost += this.materials[i].price * this.materials[i].amount
+       }
+       return cost
+    }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,23 @@
 class Material {
+    constructor(name, price, amount, units) {
+        this.name = name
+        this.price = price
+        this.amount = amount
+        this.units = units
+    }
+    useMaterial(use) {
+        if (this.amount < use) {
+        return `You don't have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+        
+    }
+    this.amount -= use
+    return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+    
+}
+calculateMaterialCost() {
+    
 
+}
 }
 
 module.exports = Material;

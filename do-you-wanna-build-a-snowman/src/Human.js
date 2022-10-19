d1ed2eb0a98135a453ee7c49af65e7656bf30ef5
jordan smith <jordan.smith6035@gmail.com>
2022-10-19 14:28:06 -0600

    Complete snowman and human test

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,36 @@
 var Snowman = require('./Snowman');
 
 class Human {
+    constructor(name) {
+        this.name = name
+        this.wantsToBuildASnowman = true
+        this.materials = {
+            snowballs: 0,
+            coal: 0,
+            buttons: 0,
+            carrots: 0
+          };
+        this.snowman = {}
+        }
 
-}
+        gatherMaterials(buildingStuff, amount) {
+        this.materials[buildingStuff] += amount
+        }
+
+        buildASnowman() {
+        var snowman = new Snowman(this.materials)
+        this.snowman = snowman
+        return snowman
+        }
+
+        placeMagicHat(snowman) {
+            if (this.snowman.coal >= 2) {
+                snowman.magicHat = true
+                return 'Woah, this snowman is coming to life!'
+            }
+            return 'I guess I didn\'t build it correctly.'
+        }
+    }
+
 
 module.exports = Human;

392d41bee2db10d8b4cd971a15dd860e5e568444
jordan smith <jordan.smith6035@gmail.com>
2022-10-24 21:45:25 -0600

    Complete lunchbox test

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,25 @@
 class LunchBox {
+    constructor(lunchDetails) {
+        this.owner = lunchDetails.owner
+        this.material = lunchDetails.madeOf
+        this.shape = lunchDetails.shape
+        this.color = lunchDetails.color
+        this.snacks = []
+        this.healthySnacks = []
+    }
 
-}
+    acquireSnack(snacks) {
+        this.snacks.push(snacks)
+        snacks.isInLunchBox = true
+    }
+    findHealthySnacks() {
+        for (var i = 0; i < this.snacks.length; i++) 
+        if (this.snacks[i].checkForHealthy()) 
+        this.healthySnacks.push(this.snacks[i].type)
+        return this.healthySnacks
+        }
+        
+    }
+
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,26 @@
 class Snack {
-
+    constructor(type) {
+        this.type = type
+        this.deliciousLevel = `extra`
+        this.amount = 100
+        this.isInLunchBox = false
+        this.cuttingItClose = false
+    }
+    getEaten() {
+        this.amount -= 10
+        var cutOffAmount = 100 * .20
+        if(this.amount <= cutOffAmount) {
+            this.cuttingItClose = true
+        }
+    }
+    checkForHealthy() {
+        var lowerCase = this.type.toLowerCase()
+        if (lowerCase.includes("fruit")){
+            return true
+        } else {
+        return false
+    }
+}
 }
 
 module.exports = Snack;
